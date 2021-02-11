2050da9107f8862aaca9946efe91836af03efdaf
MandragoraQA <76530215+MandragoraQA@users.noreply.github.com>
2021-02-11 22:32:20 +0500

    Add files via upload

---
+++ first.exp.py
@@ -0,0 +1,4 @@
+print("Nothing will work unless you do")
+phrase = "Nothing will work unless you do"
+print (phrase)
+print ("\"Anyone who\n\t\t\tstops\n\t\t\t\t\tlearning is old,\n\t\twhether at twenty or eighty”.\n\t\t\t\t\t\t\tHenry Ford")
