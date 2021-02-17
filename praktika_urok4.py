5e86ca718bb8c54f3482ca7f492f25de6edc5ef5
MandragoraQA <76530215+MandragoraQA@users.noreply.github.com>
2021-02-17 22:28:01 +0500

    Add files via upload

---
+++ praktika_urok4.py
@@ -0,0 +1,4 @@
+pervoe=int(input("Введите первую цифру: ")) #первая цифра
+vtoroe=int(input("Введите вторую цифру: ")) #вторая цифра
+tretie=int(input("Введите третью цифру: ")) #третья цифра
+print(int(pervoe),int(vtoroe),int(tretie)) #получается число из трёх цифр
