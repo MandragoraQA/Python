9eb9e8e059c821040d37f4ba578337ab1eecccde
MandragoraQA <76530215+MandragoraQA@users.noreply.github.com>
2021-02-22 22:07:48 +0500

    Add files via upload

---
+++ urok5-1.py
@@ -0,0 +1,6 @@
+#первое задание, ввести любое число
+chislo = int(input("Введите любое число: "))
+# если нечётное число
+if chislo % 2 == 1:print(int(chislo)," Odd number")
+#если чётное число
+else:print(int(chislo)," Even number")
