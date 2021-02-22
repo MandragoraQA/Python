93cb177530a3426a3562fa0c290019fd7562a541
MandragoraQA <76530215+MandragoraQA@users.noreply.github.com>
2021-02-22 22:16:58 +0500

    Add files via upload

---
+++ urok5-2.py
@@ -0,0 +1,6 @@
+#второе задание, ввести любое число
+chislo = int(input("Введите любое чиcло: "))
+#условие для кратности 7
+if chislo%7==0:print(int(chislo),"Number is multiple 7")
+#если не кратно 7
+else:print(int(chislo),"Number is not multiple 7")
