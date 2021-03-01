a56038d997f846a3c160d6a45fc0dae00bdf8e25
MandragoraQA <76530215+MandragoraQA@users.noreply.github.com>
2021-03-01 22:05:33 +0500

    Add files via upload

---
+++ urok7_1.py
@@ -0,0 +1,6 @@
+#ввести два числа
+chislo1 = int(input("Введите первое число: "))
+chislo2 =int(input("Введите второе число: "))
+for resultat in range(chislo1,chislo2):
+    print(resultat)
+print(chislo2)
